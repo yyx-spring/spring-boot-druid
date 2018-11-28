09e7fcb5f82d0c9e797463017bca08d74b1b44ec
yyx19941129 <18600094425@163.com>
2018-11-28 16:11:12 +0800

    +

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import com.bean.ApplicationProperties;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+
+    @Autowired
+    ApplicationProperties applicationProperties;
 
     @Test
     public void contextLoads() {
@@ -22,6 +27,13 @@ public class DemoApplicationTests {
             }
         }
         return index;
+    }
+
+
+    @Test
+    public void test1(){
+        System.out.println(applicationProperties.getTest1() + "/" + applicationProperties.getTest2() + "/" + applicationProperties.getTest3());
+
     }
 
 }
