1b4f27b831ad9926b33d7c0b7b8f026cf1823e7f
yyx19941129 <18600094425@163.com>
2018-12-07 18:43:38 +0800

    devtools

--- src/main/java/com/bean/TestBean1.java
+++ src/main/java/com/bean/TestBean1.java
@@ -2,11 +2,21 @@ package com.bean;
 
 import org.springframework.stereotype.Service;
 
+import javax.persistence.*;
+
 /**
  * @Author: yunxiang.yang
  * @Date: 2018/11/23 16:33
  */
 
+@Entity
+@Table(name = "testttttttt")
 public class TestBean1 {
 
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(length = 255)
+    String name;
 }
